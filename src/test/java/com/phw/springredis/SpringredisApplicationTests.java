d6f6ca78bd4464fdc42e3dc598b19030d8a708bf
sles <salessudjono@gmail.com>
2023-07-11 09:40:47 +0700

    init

---
+++ src/test/java/com/phw/springredis/SpringredisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phw.springredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringredisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
